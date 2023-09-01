c9d93028e384faaa72c319c41061a853b203c0ca
Nadzeya <radzkovanadzeya@gmail.com>
2023-09-01 17:55:02 +0300

    test

---
+++ components/other_components/card.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const Card = () => {
+    return (
+        <>
+            <div>l</div>
+            <div>price</div>
+
+        </>
+    )
+}
+export default Card;
